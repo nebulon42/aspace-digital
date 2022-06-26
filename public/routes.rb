86f5c64edf0bdd2fb5e94658e129484b3ed8f8c0
nebulon42 <nebulon42@mailbox.org>
2022-06-26 19:42:35 +0200

    update routes to avoid clash with existing aspace route

--- public/routes.rb
+++ public/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope AppConfig[:public_proxy_prefix] do
-    get '/ebook/:url' => 'ebook_reader#retrieve'
+    get '/ebook/retrieve/:url' => 'ebook_reader#retrieve'
     get '/reader' => 'ebook_reader#show'
   end
 end
